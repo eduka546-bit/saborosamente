bb5b75d51ac4ffd068e500d10b4c2bfc7da38582
eduka546-bit <eduka546@gmail.com>
2026-08-10 12:34:16 -0300

    auto: update files

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -173,7 +173,7 @@ export function SiteFooter() {
                   className="group flex items-center gap-3 opacity-80 transition-all hover:opacity-100 hover:translate-x-1"
                 >
                   <span className="flex h-9 w-9 shrink-0 items-center justify-center rounded-xl bg-white/10 transition-colors group-hover:bg-white/20">
-                    <Instagram size={16} />
+                  <InstagramIcon className="size-4" />
                   </span>
                   <span>@saborosamente</span>
                 </a>
